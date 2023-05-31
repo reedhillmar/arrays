ce781020224b6a01570109d0973878ad74e5715e
Reed Hillmar <reedhillmar@gmail.com>
2023-05-31 15:40:12 -0600

    Add explaination of index positions

--- arrays.rb
+++ arrays.rb
@@ -14,3 +14,14 @@ irrationals.unshift(0.7, 0.8, 0.9)
 
 #remove the first element from the array 'trivia_answers'
 trivia_answers.shift
+
+=begin
+Initially, array 'nfl_teams' had four elements, and "Vikings" was in index position 1.
+After running nfl_teams.pop the last element, index position 3, is removed.
+"Vikings" remains in it's initial index position.
+
+Similarly, the second instance of element "true" in array 'trivia_answers' is in index position 1.
+After running trivia_answers.shift the first element, index position 0, is removed.
+In this case, what was initially the second instance of element "true" is shifted from index position 1 to index position 0.
+=end
+
