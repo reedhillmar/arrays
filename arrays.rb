0908939dab4af1a958eda41164e4e4e2e3c6a986
Reed Hillmar <reedhillmar@gmail.com>
2023-05-31 15:31:12 -0600

    Add array methods and explainations

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ nfl_teams = ["Ravens", "Vikings", "Bears", "Lions"]
 numbers = [1,2,3,4]
 irrationals = [1.1, 1.2, 1.3, 1.4]
 trivia_answers = [true, true, false, true]
+
+#remove the last element from the array 'nfl_teams'
+nfl_teams.pop
+
+#add the element "5" to the end of array 'numbers'
+nfl_teams.push(5)
+
+#add the elements "0.7, 0.8, and 0.9" to the beginning of array 'irrationals'
+irrationals.unshift(0.7, 0.8, 0.9)
+
+#remove the first element from the array 'trivia_answers'
+trivia_answers.shift
